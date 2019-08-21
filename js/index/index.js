e7ff95cfd201329e8fd2cbfec693545023306c5e
陈亮 <1109107755@qq.com>
2019-08-21 14:29:25 +0800

    上一页下一页功能基本完成，但仍然需要完善

--- js/index/index.js
+++ js/index/index.js
@@ -1,14 +1,56 @@
-$(function () {
-    var pageIndex = 1;
-    var pageNum = 6;
-    let data;
-    let columnList;
-    let columnBtn;
-    let articleList;
-    var hurl = "http://localhost:8888/sys/front/index";
+// 当前页
+let curPage = 1;
+// 每页显示多少条数据
+let pageSize = 5;
+let data;
+let columnList;
+let columnBtn;
+let articleTile;
 
+$(function () {
+
+
+    loadIndex();
+
+    /**
+     * 下一页
+     */
+    $("#id-a-next").click(function () {
+        ++curPage;
+        alert(curPage);
+        $("#id-div-articleList").html("");
+        loadIndex();
+    });
+
+    /**
+     * 上一页
+     */
+    $("#id-a-prev").click(function () {
+        if (curPage <= 1) {
+            alert("已是第一页");
+        } else {
+            --curPage;
+            $("#id-a-prev").attr("disabled", false);
+            alert(curPage);
+            $("#id-div-articleList").html("");
+            loadIndex();
+        }
+    });
+
+});
+
+/**
+ * 初始化
+ */
+function loadIndex() {
+    var hurl = "http://localhost:8888/sys/front/index";
     $.ajax({
+        type: "POST",
         url: hurl,
+        data: {
+            curPage: curPage,
+            pageSize: pageSize
+        },
         success: function (e) {
             var result = eval("(" + e + ")");
             console.log("e:", result);
@@ -28,10 +70,10 @@ $(function () {
              */
             $("#id-a-nav-one").text(columnList[0].sysName);
             $("#id-a-index").attr("href", columnList[0].sysValue);
-            $("#id-a-nav-two").text(columnList[1].sysName);
-            $("#id-a-blogList").attr("href", columnList[1].sysValue);
-            $("#id-a-nav-three").text(columnList[2].sysName);
-            $("#id-a-about").attr("href", columnList[2].sysValue);
+            // $("#id-a-nav-two").text(columnList[1].sysName);
+            // $("#id-a-blogList").attr("href", columnList[1].sysValue);
+            $("#id-a-nav-three").text(columnList[1].sysName);
+            $("#id-a-about").attr("href", columnList[1].sysValue);
 
             /**
              * 首页导航栏按钮
@@ -47,65 +89,73 @@ $(function () {
             $.each(articleList, function (i, val) {
                 $("#id-div-articleList").append("<div class='item mb-5'>" +
                     "<div class='media'>" +
-                    "<input type='hidden' id='id-article'>" +
                     "<img id='id-img-titleImg' class='mr-3 img-fluid post-thumb d-none d-md-flex' alt='image'>" +
                     "<div class='media-body'>" +
-                    "<h3 class='title mb-1'><a href='blog-post.html' id='id-h3-title'></a></h3>" +
-                    "<div class='meta mb-1'><span class='date' id='id-span-time'></span><span class='time' id='id-span-clickCount'>" +
-                    "</span><span class='comment'><a id='id-span-comment'></a></span></div>" +
+                    "<h3 class='title mb-1'><a href='javascript:void(0)' id='id-h3-title'></a></h3>" +
+                    "<div class='meta mb-1'><span class='date' id='id-span-time'></span>" +
+                    "<span class='time' id='id-span-clickCount'></span>" +
+                    "<span class='comment'><a id='id-span-comment'></a></span></div>" +
                     "<div class='intro' id='id-div-artContent'></div>" +
-                    "<a class='more-link' href='blog-post.html'>阅读更多 &rarr;</a>" +
                     "</div>" +
                     "</div>" +
                     "</div>");
                 $("#id-article").attr("id", "id-article" + i);
                 $("#id-article" + i).attr("value", articleList[i].artId);
+
                 $("#id-img-titleImg").attr("id", "id-img-titleImg" + i);
                 $("#id-img-titleImg" + i).attr("src", articleList[i].artTitleImgURL);
+
                 $("#id-h3-title").attr("id", "id-h3-title" + i);
+                $("#id-h3-title" + i).click(function () {
+                    window.location.href = "blog-post.html?artId=" + articleList[i].artId;
+                });
+
                 $("#id-h3-title" + i).text(articleList[i].artTitle);
                 $("#id-span-time").attr("id", "id-span-time" + i);
                 $("#id-span-time" + i).html(subCreateTime(articleList[i].artCreateTime));
                 $("#id-span-clickCount").attr("id", "id-span-clickCount" + i);
-                $("#id-span-clickCount" + i).html(articleList[i].artClickCount + "人浏览");
+                $("#id-span-clickCount" + i).html("浏览次数：" + articleList[i].artClickCount);
                 $("#id-span-comment").attr("id", "id-span-comment" + i);
                 $("#id-span-comment" + i).html(articleList[i].artComment + "人评论");
                 $("#id-div-artContent").attr("id", "id-div-artContent" + i);
                 $("#id-div-artContent" + i).html(subContent(articleList[i].artContent));
+                $("#id-artTitle").html(articleTile);
             });
         }
     });
+}
 
+/**
+ * 截取时间到分（省略秒）
+ * @param artCreateTime
+ * @returns {string}
+ */
+function subCreateTime(artCreateTime) {
+    return artCreateTime.substring(0, 16);
+}
 
-    function searchArticle() {
-        alert(1);
-        var value = $("#semail").val();
-        var hurl = "http://localhost:8888/sys/article/getArticleByCondition";
-        $.post(
-            hurl,
-            {"condition": value},
-            function (e) {
-                console.log(eval("(" + e + ")"));
-            }
-        );
-    }
-    /**
-     * 截取时间到分（省略秒）
-     * @param artCreateTime
-     * @returns {string}
-     */
-    function subCreateTime(artCreateTime) {
-        return artCreateTime.substring(0, 16);
-    }
+/**
+ * 截取文章前95个字（省略秒）
+ * @param artCreateTime
+ * @returns {string}
+ */
+function subContent(artContent) {
+    var str = artContent.substring(0, 120);
+    return str + "...";
+}
 
-    /**
-     * 截取文章前95个字（省略秒）
-     * @param artCreateTime
-     * @returns {string}
-     */
-    function subContent(artContent) {
-        var str = artContent.substring(0, 95);
-        return str + "...";
-    }
-});
-
+/**
+ * 文章搜索
+ */
+function searchArticle() {
+    alert(1);
+    var value = $("#semail").val();
+    var hurl = "http://localhost:8888/sys/article/getArticleByCondition";
+    $.post(
+        hurl,
+        {"condition": value},
+        function (e) {
+            console.log(eval("(" + e + ")"));
+        }
+    );
+}
