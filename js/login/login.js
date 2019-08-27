ca89db0ae0cb8b23b48db1c414b2930244fc5b57
陈亮 <1109107755@qq.com>
2019-08-27 13:34:57 +0800

    登录功能完成

--- js/login/login.js
+++ js/login/login.js
@@ -2,18 +2,26 @@
  * 博客登录js
  */
 
+let codeImgUrl = "http://localhost:8888/kaptcha";
+let url = "http://localhost:8888/sys/user/login";
+
 $(function () {
+
+    // 提示框隐藏
     $("#id-msg").hide();
+
+    // 获取验证码图片
+    getCodeImg();
 
     /**
      * 登录(ajax表单请求)
      */
-    $("#id-login").on('click', function () {
-        let hurl = "http://localhost:8888/sys/user/login";
+    $("#id-login").click(function () {
         $.ajax({
-            url: hurl,
+            url: url,
             type: "post",
             data: $("#id-form").serialize(),
+            withCredentials: true,
             success: function (e) {
                 let data = JSON.parse(e);
                 if (data.type == "success") {
@@ -25,12 +33,33 @@ $(function () {
                 }
             },
         });
-    })
+    });
+
 });
 
-// function resizeBannerImage() {
-//     var winW = $(window).width();
-//     if ($(window).width() > 450) {
-//         $("#id-img").attr('display', 'none');
-//     }
-// }
+
+
+/**
+ * 获取验证码图片
+ */
+function getCodeImg() {
+    $.ajax({
+        url: codeImgUrl,
+        type: 'post',
+        success: function (e) {
+            let data = JSON.parse(e);
+            $("#captcha_img").attr("src", "data:image/jpg;base64," + data.codeImg);
+            $("#session_id").attr("value", data.sessionId);
+        },
+    })
+}
+
+
+/**
+ * 验证码刷新
+ */
+function refresh() {
+    // 提示框隐藏
+    $("#id-msg").hide();
+    getCodeImg();
+}
